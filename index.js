77235a613adb7148c035eafb8b1eb41ab8f5ff8e
Daniel Rosel <daniel@alves.world>
2023-08-30 13:12:18 +0200

    setup for better structure

--- index.js
+++ index.js
@@ -1,33 +1,64 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const openai = require('openai');
+const openaiApiKey = core.getInput('openai_api_key');
+const githubToken = core.getInput('github_token');
+const octokit = github.getOctokit(githubToken);
 
-console.log('Hello world!');
+async function aiDiffAnalsis(diffs) {
+    const prompt = `Your job is to review the following PR and determine if it is spam or not. It is currently hacktober-fest and there is a lot of spam going around. Many users are creating PRs that do not contribute anything very valuable. Here is the PR diff: \n ${diffs} \n Is this PR spam or not? Return one of the following:\nSpam\nNot spam`;
+    const messagesPayload =  [
+        { role: 'system', content: 'You are a helpful assistant.' },
+        { role: 'user', content: prompt }
+    ];
+
+    // get the key from the github secret
+    const openaiApiKey = core.getInput('openai_api_key');
+    const openaiClient = new openai(openaiApiKey);
+
+
+    const openai = new OpenAI();
+
+    const response = await openai.chat.completions.create({
+        model: 'gpt-4',
+        messages: messagesPayload,
+    });
+
+    let textual = response.choices[0];
+
+    console.log(textual);
+
+}
+
+async function spamRegistry(actor) {
+
+    let diffs = '', spamLikelihood = 0;
+
+    try {
+        // fetch the spam likelihood
+        let response = await fetch(`https://790a-2a0c-5a80-1f10-3f00-2506-17cf-1cfe-ba07.ngrok-free.app/check?username=${actor}`);
+        response = await response.text();
+        console.log(response);
+        if (response !== 'Not spam') {
+            spamLikelihood = response.split('%')[0];
+        } else {
+            spamLikelihood = 0;
+        }
+
+    } catch (error) {
+
+    }
+    return spamLikelihood;
+}
+
+async function getDiffs() {
+}
 
 async function run() {
     try {
-        const openaiApiKey = core.getInput('openai_api_key');
-        const githubToken = core.getInput('github_token');
-        const octokit = github.getOctokit(githubToken);
         const actor = github.context.actor;
 
-        console.log(`Actor: ${actor}`);
-
-        let diffs = '', spamLikelihood = 0;
-
-        try {
-            // fetch the spam likelihood
-            let response = await fetch(`https://790a-2a0c-5a80-1f10-3f00-2506-17cf-1cfe-ba07.ngrok-free.app/check?username=${actor}`);
-            response = await response.text();
-            console.log(response);
-            if (response !== 'Not spam') {
-                spamLikelihood = response.split('%')[0];
-            } else {
-                spamLikelihood = 0;
-            }
-
-        } catch (error) {
-
-        }
+        let spamLikelyhood = await spamRegistry(actor);
 
         try {
             // fetch the diffs
@@ -46,12 +77,8 @@ async function run() {
 
         }
 
-        const prompt = `Your job is to review the following PR and determine if it is spam or not. It is currently hacktober-fest and there is a lot of spam going around. Many users are creating PRs that do not contribute anything very valuable. Here is the PR diff: \n ${diffs} \n Is this PR spam or not? Return one of the following:\nSpam\nNot spam`;
 
-        const messagesPayload = JSON.stringify([
-            { role: 'system', content: 'You are a helpful assistant.' },
-            { role: 'user', content: prompt }
-        ]);
+
 
         console.log(`Messages payload: ${messagesPayload}`);
 
